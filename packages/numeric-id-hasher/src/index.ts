1ce35735e89c969984e1f5a7aa6bd4864f348b1a
busticated <itsbusticated@gmail.com>
2023-12-08 13:06:19 -0800

    [numeric-id-hasher] add initial implementation

--- packages/numeric-id-hasher/src/index.ts
+++ packages/numeric-id-hasher/src/index.ts
@@ -1,3 +1,37 @@
-export const greet = () => {
-	return 'hello world!';
+const CHAR_TABLE = '23456789ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnpqrstuvwxyz';
+const BASE = BigInt(CHAR_TABLE.length);
+const IS_ONLY_DIGITS_PTN = /^\d*$/;
+const ZERO = BigInt(0);
+
+
+export const encode = (id: string) => {
+	if (!IS_ONLY_DIGITS_PTN.test(id)){
+		throw new Error('only ids with digits 0-9 can be encoded');
+	}
+
+	let num = BigInt(id);
+	const result = [];
+
+	do {
+		const idx = Number(num % BASE);
+		result.push(CHAR_TABLE[idx]);
+		num = num / BASE;
+	} while (num > ZERO);
+
+	return result.reverse().join('');
 };
+
+export const decode = (hash: string) => {
+	let result = BigInt(0);
+	let charIndex, i;
+
+	hash = hash.split('').reverse().join('');
+
+	for (i = 0; i < hash.length; i++){
+		charIndex = BigInt(CHAR_TABLE.indexOf(hash.charAt(i)));
+		result = (BASE ** BigInt(i)) * charIndex + result;
+	}
+
+	return result.toString();
+};
+
