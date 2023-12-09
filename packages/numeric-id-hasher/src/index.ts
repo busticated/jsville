f16a0d27533fcb7c437d16da634c73409f6a530e
busticated <itsbusticated@gmail.com>
2023-12-09 15:09:59 -0800

    [numeric-id-hasher] add docs

--- packages/numeric-id-hasher/src/index.ts
+++ packages/numeric-id-hasher/src/index.ts
@@ -1,9 +1,30 @@
+/**
+ * Converts a large stringified number into a hash - handy for creating shortened
+ * urls like:
+ *
+ * ```
+ * example.com/u/67458932454109883456 -> example.com/u/7vy6d8rw4SPh
+ * ```
+ *
+ * @packageDocumentation
+ */
+
+
 const CHAR_TABLE = '23456789ABCDEFGHJKLMNPRSTUVWXYZabcdefghjkmnpqrstuvwxyz';
 const BASE = BigInt(CHAR_TABLE.length);
 const IS_ONLY_DIGITS_PTN = /^\d*$/;
 const ZERO = BigInt(0);
 
 
+/**
+ * Encodes your id
+ *
+ * @example
+ * ```ts
+ * import { encode } from '@bust/numeric-id-hasher';
+ * encode('67458932454109883456'); // '7vy6d8rw4SPh'
+ * ```
+ */
 export const encode = (id: string) => {
 	if (!IS_ONLY_DIGITS_PTN.test(id)){
 		throw new Error('only ids with digits 0-9 can be encoded');
@@ -21,6 +42,15 @@ export const encode = (id: string) => {
 	return result.reverse().join('');
 };
 
+/**
+ * Decodes your hash back into the originally provided id
+ *
+ * @example
+ * ```ts
+ * import { decode } from '@bust/numeric-id-hasher';
+ * decode('7vy6d8rw4SPh'); // '67458932454109883456'
+ * ```
+ */
 export const decode = (hash: string) => {
 	let result = BigInt(0);
 	let charIndex, i;
